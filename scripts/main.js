fee5b6afbfdea455958f7fdf95b18239a8404536
maliel8 <labagama81@gmail.com>
2023-08-06 21:20:58 +0100

    partie principale du jeux uniquement les mots sont gerées ici les interaction avec le bouton valider le champ de saisie la zone de score la zone de proposition de mots .. la suite conserne: le choix du type de proposition (phrases ou mots)

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,3 @@
-/* fichier principal : point d'entrer de notre jeu */
-let zone_saisie = document.getElementById('entre_ecriture');
-let bouton_valider = document.getElementById('bouton_valider_ecrit');
-let zone_proposition = document.querySelector(".zone_proposition");
-let zone_score = document.querySelector(".zone_score span");
-let option_source = document.querySelectorAll(".option_source input");
 
 lancer_jeu();
 
--- scripts/script.js
+++ scripts/script.js
@@ -1,56 +1,57 @@
 
 /* fichier contenant toutes les fonction de notre jeu  */
+let option_source = document.querySelectorAll(".option_source input");
 
 // c'est la fonction principale
 function lancer_jeu() 
 {
-      let choix = choisir_phrases_ou_mots();
       let score = 0;
-      let nbr_tt_proposer = 0;
+      let i = 0;
 
-      if(choix === "mots")
-      {
-            score = lancer_boucle_de_jeu(list_mots);
-            nbr_tt_proposer = list_mots.length;
-      }
-      else 
-      {
-            score = lancer_boucle_de_jeu(liste_phrases);
-            nbr_tt_proposer = liste_phrases.length;
-      }
+      let bouton_valider = document.getElementById('bouton_valider_ecrit');
+      let zone_saisie = document.getElementById('entre_ecriture');
 
-     affiche_resultat(score,nbr_tt_proposer);
+      affiche_proposition(liste_mots[i]); 
+      bouton_valider.addEventListener("click",()=>{
+
+      if(liste_mots[i].localeCompare(zone_saisie.value) == 0)
+             {
+                      score++;
+                }
+                  i++;
+            affiche_resultat(score,i);
+            zone_saisie.value= "";   
+            if(liste_mots[i] === undefined )
+            {
+                  let fin ="le jeux est fini";
+                  affiche_proposition(fin);
+                  bouton_valider.disabled = true;//desactivation du bouton 
+            }
+            else
+            {
+                  affiche_proposition(liste_mots[i]);
+            }
+      });
+      
+      affiche_resultat(score,i);
+      
 
 } 
+
+function affiche_proposition(proposition)
+{
+      let zone_proposition = document.querySelector(".zone_proposition");
+      zone_proposition.innerText = proposition;
+}
 
 // fonction affichant le nombre de reponce juste trouver
 function affiche_resultat(score,nbr_tt_proposer)
 {
-      console.log("votre score est de : "+score+" sur "+nbr_tt_proposer);
+      //console.log("votre score est de : "+score+" sur "+nbr_tt_proposer);
+      let zone_score = document.querySelector(".zone_score span");
+      let affiche_score = `${score} / ${nbr_tt_proposer}`;
+      zone_score.innerText = affiche_score;
 }
 
-//fonction de choix du type de jeu (mots ou phrase)
-function choisir_phrases_ou_mots()
-{
-      let choix = prompt(" avec quelle liste desirez vous jouer : 'mots' ou 'phrases'");
-      while(choix !== "mots" && choix !== "phrases")
-      {
-            choix = prompt("avec quelle liste voulez vous jouer : 'mots' ou 'phrases' ");
-      }
-      return choix;
-}
 
-// fonction de test elle compare la proposition de l'utilisateur et celle proposer par le jeu
-function lancer_boucle_de_jeu(liste)
-{
-      let score = 0;
-      for(let i = 0 ; i < liste.length ; i++)
-      {
-            let proposition_utilisateur = prompt("entrez la valeur ==> " +liste[i]);
-            if(proposition_utilisateur === liste[i])
-            {
-                  score ++;
-            }
-      }
-      return score;
-}
+
